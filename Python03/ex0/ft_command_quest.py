d7a2284055cf7e38c57699b7c3a2e1237259febb
kapustazh <51422901+kapustazh@users.noreply.github.com>
2025-12-18 18:32:54 +0100

    Ex0 and start of the ex1.

---
+++ Python03/ex0/ft_command_quest.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def ft_command_quest():
+    print("=== Command Quest ===")
+    if len(sys.argv) < 2:
+        print("No arguments provided!")
+    print(f"Program name: {sys.argv[0]}")
+    if len(sys.argv) > 1:
+        print(f"Arguments recieved: {len(sys.argv) - 1}")
+    count = 1
+    for arg in sys.argv[1:]:
+        print(f"Argument {count}: {arg}")
+        count += 1
+    print(f"Total arguments: {len(sys.argv)}")
+    # Authorized: import sys, sys.argv, len(), print()
+
+
+if __name__ == "__main__":
+    ft_command_quest()
