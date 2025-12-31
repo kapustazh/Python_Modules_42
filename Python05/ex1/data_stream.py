c4fd11297f79513e7a38f71d25da0bbacd056821
kapustazh <51422901+kapustazh@users.noreply.github.com>
2025-12-31 23:17:18 +0100

    Started doing ex1, have done baseclasses

--- Python05/ex1/data_stream.py
+++ Python05/ex1/data_stream.py
@@ -1,1 +1,51 @@
 #!/usr/bin/env python3
+from abc import ABC, abstractmethod
+from typing import Any, List, Dict, Union, Optional
+
+
+class DataStream(ABC):
+
+    @abstractmethod
+    def process_batch(self, data_batch: List[Any]) -> str:
+        pass
+
+    def filter_data(
+        self, data_batch: List[Any], criteria: Optional[str] = None
+    ) -> List[Any]:
+        return []
+
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+        return {}
+
+
+class SensorStream(DataStream):
+    def process_batch(self, data_batch: List[Any]) -> str:
+        return ""
+
+
+class TransactionStream(DataStream):
+    def process_batch(self, data_batch: List[Any]) -> str:
+        return ""
+
+
+class EventStream(DataStream):
+    def process_batch(self, data_batch: List[Any]) -> str:
+        return ""
+
+
+class StreamProcessor:
+    pass
+
+
+def data_stream_py() -> None:
+    try:
+        print("=== CODE NEXUS - POLYMORPHIC STREAM SYSTEM ===")
+    except Exception as e:
+        print(f"Something went wrong -> {e}")
+
+
+if __name__ == "__main__":
+    pass
+
+# Authorized: class, def, super(), isinstance(), print(), try/except, list
+# comprehensions,
